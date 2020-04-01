0e7189eddd2ada56f82ab8a9f4a6552814c76477
morbi <the_morbidus@hotmail.com>
2020-04-01 16:25:30 -0400

    Form for theme and difficulty added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Jumbotron, Button } from 'react-bootstrap'; 
+import { Jumbotron, Button, Form } from 'react-bootstrap'; 
 
 function Home () {
+  const [theme, setTheme] = useState("Dogs");
+  const [diff, setDiff] = useState("Easy");
+
+  let onSubmit = (e) => {
+    e.preventDefault();
+    console.log(theme);
+    console.log(diff);
+
+  }
+
 
 
   return (
-    <Jumbotron>
-      <h1>Pair it!!</h1>
-      <h2>the game</h2>
-      <Button variant="primary" href="/game">Let's play!!</Button>
-    </Jumbotron>
+    <div>
+      <Jumbotron>
+        <h1>Pair it!!</h1>
+        <h2>the game</h2>
+      </Jumbotron>
+      <Form onSubmit={onSubmit}>
+        <Form.Group controlId="theme">
+          <Form.Label>Choose Theme</Form.Label>
+          <Form.Control as="select" value={theme} onChange={e => {
+            setTheme(e.target.value)
+          }}>
+            <option>Dogs</option>
+            <option>Cats</option>
+            <option>Cars</option>
+            <option>Waifus</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Choose Difficulty</Form.Label>
+          <Form.Control as="select" value={diff} onChange={e => {
+            setDiff(e.target.value)
+          }}>
+            <option>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">Play!!</Button>
+      </Form>
+    </div>
+    
   );
 }
 
